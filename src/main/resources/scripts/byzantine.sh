45321b2c94236a0f5c6da22f071f777747722b88
Brett Dutro <dutro2@illinois.edu>
2014-05-06 19:32:28 -0500

    - Removed byzantine binary - Fixed byzantine script to run properly as root in background

--- src/main/resources/scripts/byzantine.sh
+++ src/main/resources/scripts/byzantine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-START_IP=10.0.0.3
-END_IP=10.0.0.5
-iptables -A OUTPUT -m iprange --dst-range $START_IP-$END_IP -j NFQUEUE --queue-num 0
-nohup /tmp/byzantine_monkey &
+START_IP=10.0.0.9
+END_IP=10.0.0.10
+sudo iptables -A OUTPUT -m iprange --dst-range $START_IP-$END_IP -j NFQUEUE --queue-num 0
+nohup sudo -b /tmp/byzantine_monkey
