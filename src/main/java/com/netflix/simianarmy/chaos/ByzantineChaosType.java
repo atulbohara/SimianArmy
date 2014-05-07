6b6936408c575921a8bedde3b6e03bd661fb637b
Brett Dutro <dutro2@illinois.edu>
2014-05-06 20:24:07 -0500

    - Set up properties files for Byzantine - Fixed SSH put to use byte array instead of string

--- src/main/java/com/netflix/simianarmy/chaos/ByzantineChaosType.java
+++ src/main/java/com/netflix/simianarmy/chaos/ByzantineChaosType.java
@@ -3,11 +3,13 @@ package com.netflix.simianarmy.chaos;
 import java.io.IOException;
 import java.net.URL;
 
+import org.jclouds.io.payloads.ByteSourcePayload;
 import org.jclouds.ssh.SshClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Charsets;
+import com.google.common.io.ByteSource;
 import com.google.common.io.Resources;
 import com.netflix.simianarmy.MonkeyConfiguration;
 
@@ -27,14 +29,14 @@ public class ByzantineChaosType extends ScriptChaosType {
 
         String filename = getKey().toLowerCase();
         URL url = Resources.getResource(ScriptChaosType.class, "/scripts/" + filename);
-        String script;
+        byte[] binary;
         try {
-            script = Resources.toString(url, Charsets.UTF_8);
+        	binary = Resources.toByteArray(url);
         } catch (IOException e) {
             throw new IllegalStateException("Error reading script resource", e);
         }
-
-        ssh.put("/tmp/" + filename, script);
+        
+        ssh.put("/tmp/" + filename, new ByteSourcePayload(ByteSource.wrap(binary)));
         
         super.apply(instance);
 	}
